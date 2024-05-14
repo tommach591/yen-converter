a1f187afeb105aa443b32c95e95b28aa46cdb6e2
Tom Mach <57111473+tommach591@users.noreply.github.com>
2024-05-13 20:46:06 -0700

    made input more obvious and updated groupings

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -9,18 +9,21 @@ function Table({ JPYRates }) {
   const [currency, setCurrency] = useState("USD");
   const listOfCurrency = ["USD", "CAD", "AUD", "EUR", "CNY"];
 
-  const set1 = Array.from({ length: 9 }, (_, i) => (i + 1) * 100);
-  const set2 = Array.from({ length: 18 }, (_, i) => 1000 + i * 500);
-  const set3 = Array.from({ length: 15 }, (_, i) => 10000 + i * 1000);
-  const set4 = Array.from({ length: 25 }, (_, i) => 25000 + i * 1000);
-  const set5 = Array.from({ length: 25 }, (_, i) => 50000 + i * 1000);
-  const set6 = Array.from({ length: 25 }, (_, i) => 75000 + i * 1000);
-  const set7 = Array.from({ length: 15 }, (_, i) => 100000 + i * 10000);
-  const set8 = Array.from({ length: 25 }, (_, i) => 250000 + i * 10000);
-  const set9 = Array.from({ length: 25 }, (_, i) => 500000 + i * 10000);
-  const set10 = Array.from({ length: 26 }, (_, i) => 750000 + i * 10000);
-
-  const sets = [set1, set2, set3, set4, set5, set6, set7, set8, set9, set10];
+  const sets = [
+    Array.from({ length: 9 }, (_, i) => (i + 1) * 100),
+    Array.from({ length: 8 }, (_, i) => 1000 + i * 500),
+    Array.from({ length: 10 }, (_, i) => 5000 + i * 500),
+    Array.from({ length: 10 }, (_, i) => 10000 + i * 1000),
+    Array.from({ length: 10 }, (_, i) => 20000 + i * 1000),
+    Array.from({ length: 10 }, (_, i) => 30000 + i * 1000),
+    Array.from({ length: 10 }, (_, i) => 40000 + i * 1000),
+    Array.from({ length: 10 }, (_, i) => 50000 + i * 5000),
+    Array.from({ length: 10 }, (_, i) => 100000 + i * 10000),
+    Array.from({ length: 10 }, (_, i) => 200000 + i * 10000),
+    Array.from({ length: 10 }, (_, i) => 300000 + i * 10000),
+    Array.from({ length: 10 }, (_, i) => 400000 + i * 10000),
+    Array.from({ length: 11 }, (_, i) => 500000 + i * 50000),
+  ];
 
   useEffect(() => {
     const storedData = localStorage.getItem("currency");
