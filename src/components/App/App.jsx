625f0c44b55812aeaa9defc5082adbe182a6d5c1
Tom Mach <57111473+tommach591@users.noreply.github.com>
2024-06-01 16:53:28 -0700

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -8,6 +8,7 @@ function App() {
 
   /* Set JPY Rates on load */
   useEffect(() => {
+    console.log(localStorage.getItem("JPYRates"));
     const storedData =
       localStorage.getItem("JPYRates") === undefined
         ? JSON.parse(localStorage.getItem("JPYRates"))
